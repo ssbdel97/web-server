f18c8cbcfd6c41cb5361560873b995820464ef41
Siddarth Bisht <ssbdel97@gmail.com>
2018-01-11 00:23:19 +0530

    Initialise repo

---
+++ server.js
@@ -0,0 +1,25 @@
+var express =require('express');
+var app = express();
+// app.get('/',function(req,res){
+// 	res.send('Hello Express');
+// });
+var port = 3000;
+var middleWare={
+	requireAuthentication: function(req,res,next){
+		console.log('private route hit!');
+		next();
+	},
+	logger: function(req,res,next){
+		console.log('Request: '+ new Date().toString() + ' ' + req.method+' '+req.originalUrl);
+		next();
+	} 
+};
+app.use(middleWare.logger);
+app.get('/about',middleWare.requireAuthentication,function(req,res){
+	res.send('About Us');
+});
+app.use(express.static(__dirname+'/public'));
+//console.log(__dirname);
+app.listen(port,function(){
+	console.log('Server Started '+port);
+});
